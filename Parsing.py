a46c62263f961135c6120092ed0d2bc2d1d01752
Gabriel Hope <Gabriel@gabriels-air.wufi.wustl.edu>
2013-11-15 14:59:32 -0600

    Actually finished parsing

--- Parsing.py
+++ Parsing.py
@@ -3,6 +3,7 @@ from scipy.sparse import *
 from HTMLParser import HTMLParser
 import re
 import string
+import itertools
 
 #Thanks Eloff from StackOverflow!
 class MLStripper(HTMLParser):
@@ -19,14 +20,37 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
+def save_matrix(filename, matrix):
+	with open(filename, 'w') as mat_writer:
+		cx = coo_matrix(matrix)
+		mat_writer.write(str(cx.shape[0]) + ' ' + str(cx.shape[1]) + '\n')
+		for i,j,v in itertools.izip(cx.row, cx.col, cx.data):
+			mat_writer.write("%d %d %s" % (i,j,v) + '\n')
+
+def load_matrix(filename):
+	with open(filename, 'rU') as mat_reader:
+		line = mat_reader.readline().split()
+		rows = int(line[0])
+		cols = int(line[1])
+		matrix = lil_matrix((rows, cols))
+		for line in mat_reader:
+			line = line.split()
+			row = int(line[0])
+			col = int(line[1])
+			val = float(line[2])
+			matrix[row, col] = val
+		return matrix.tocsr()
 
 class BagOfWords(object):
 	"""docstring for BagOfWords"""
 
-	def __init__(self, training_filename, testing_filename, stop_filename='', n=2, tf_idf=True):
+	def __init__(self, training_filename, testing_filename, stop_filename='', n=2, tf_idf=True, combine_code=True):
 		self.ngram_map = {}
 		self.ngram_list = []
 		self.tags = set()
+		self.tag_list = []
+		self.tag_map = {}
+		self.num_tags = 0
 		self.training_set = set()
 		self.test_set = set()
 		self.ngram_set = set()
@@ -40,12 +64,17 @@ class BagOfWords(object):
 		self.tf_idf_map = {}
 		self.stop_words = set()
 		self.n = n
+		self.combine_code = combine_code
 
 		self.build_stop_set(stop_filename)
 		self.build_tag_list(training_filename)
 		self.build_bag_of_words(training_filename, testing_filename, self.n)
-		print self.training_bag
-		print self.testing_bag
+		save_matrix("training_matrix.txt", self.training_bag.todense())
+		save_matrix("testing_matrix.txt", self.testing_bag)
+		save_matrix("training_tag_matrix.txt", self.training_tags)
+		#print load_matrix("training_matrix.txt")
+		#print load_matrix("testing_matrix.txt")
+		#print load_matrix("training_tag_matrix.txt")
 
 	def build_stop_set(self, stop_filename):
 		if stop_filename == '':
@@ -62,8 +91,14 @@ class BagOfWords(object):
 
 	def strip_code_and_formatting(self, input):
 		codeList = re.split(r'<code>|</code>', input)
-		text = ' '.join(codeList[0::2])
-		code = ' '.join(codeList[1::2])
+		text = ''
+		code = ''
+		if not self.combine_code:
+			text = ' '.join(codeList[0::2])
+			code = ' '.join(codeList[1::2])
+		else:
+			text = ' '.join(codeList)
+
 		text = strip_tags(text)
 		text = text.translate(string.maketrans("",""), string.punctuation)
 		return text.lower(), code
@@ -129,6 +164,14 @@ class BagOfWords(object):
 		qID = int(question[0][1:])
 		qTitle = question[1]
 		qBody = question[2]
+
+		if training:
+			qTags = question[3].lower()[:-1]
+			qTags = qTags.split()
+			for qTag in qTags:
+				training_ind = self.training_question_index
+				tag_index = self.tag_map[qTag]
+				self.training_tags[training_ind, tag_index] = 1
 
 		qTitle, qTCode = self.strip_code_and_formatting(qTitle)
 		qBody, qBCode = self.strip_code_and_formatting(qBody)
@@ -227,6 +270,7 @@ class BagOfWords(object):
 
 		self.training_bag = lil_matrix((self.training_questions, next_index))
 		self.testing_bag =  lil_matrix((self.testing_questions, next_index))
+		self.training_tags = lil_matrix((self.training_questions, self.num_tags))
 
 		with open(training_filename, 'rU') as data_reader:
 			question = ''
@@ -262,8 +306,18 @@ class BagOfWords(object):
 					self.process_tags(question)
 					question = ''
 
+		self.tag_list = []
+
+		next_index = 0
+		for tag in self.tags:
+			self.tag_map[tag] = next_index
+			self.tag_list.append(tag)
+			next_index += 1
+
 		with open('tag_list.txt', 'w') as tag_writer:
-			tag_writer.write('\n'.join(self.tags))
+			tag_writer.write('\n'.join(self.tag_list))
+
+		self.num_tags = next_index
 
 
 def main():
